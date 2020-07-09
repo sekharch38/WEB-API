df5ef14b5392e29846ffd3fbd8d2475af0f5e65b
Anusha Nilapu <anusha.nilapu@gmail.com>
2020-07-09 21:23:26 +0530

    donesuccessfully

---
+++ WEB-API_SEARCH/Controllers/SearchController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Http;
+using WEB_API_SEARCH.Models;
+
+namespace WEB_API_SEARCH.Controllers
+{
+    public class SearchController : Controller
+    {
+        //
+        // GET: /Search/
+        public ActionResult Index( string search)
+        {
+            IEnumerable<Employee> obj =null ;
+            HttpClient hc = new HttpClient();
+            hc.BaseAddress = new Uri("http://localhost:52885/api/");
+            var consumeapi = hc.GetAsync("SearchEmployee?search=" + search);
+            consumeapi.Wait();
+            var readData = consumeapi.Result;
+            if (readData .IsSuccessStatusCode)
+            {
+                var displayData = readData.Content.ReadAsAsync<List<Employee>>();
+                displayData.Wait();
+                obj = displayData.Result;
+
+            }
+
+            return View(obj);
+        }
+	}
+}
---
+++ WEB-API_SEARCH/Controllers/SearchEmployeeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WEB_API_SEARCH.Models;
+
+namespace WEB_API_SEARCH.Controllers
+{
+    public class SearchEmployeeController : ApiController
+    {
+        public IHttpActionResult getempname(string search)
+        {
+            EmployeeEntities entities = new EmployeeEntities();
+            var result = entities.Employees.Where(x => x.First_Name.StartsWith(search) || search == null).ToList();
+            return Ok(result);
+        }
+    }
+}
